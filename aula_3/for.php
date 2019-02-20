c63dd301667943d5b95ce207af26b44c3bc4c3c5
Bruners-1308 <43767231+Bruners-1308@users.noreply.github.com>
2019-02-20 20:25:55 -0300

    Add files via upload

---
+++ aula_3/for.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		
+	</head>
+		<body>
+		
+			<?php
+						
+			$soma=0;
+			$i;
+			
+			for ($i = 1; $i <=100; $i++){
+				($soma+=$i);
+			}
+			echo " " .$soma;
+			?>
+			
+			
+			
+		</body>
+<html>
---
+++ aula_3/index.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		
+	</head>
+		<body>
+		
+			<?php
+			/**	$i = 1;
+				while ($i <= 10){
+					//echo " ";
+					//echo $i++;
+					echo " ".$i++;
+				}
+				**/
+				
+			/**	$i =1;
+				do{
+					echo " ";
+					echo $i++;
+				}
+				while ($i <=10);
+			**/
+			
+			for ($i = 0; $i <=10; $i++){
+				echo " " .$i;
+			}
+			?>
+			
+			
+			
+		</body>
+<html>
