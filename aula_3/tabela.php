26633fd067ee430ad26a49c37d07c38d53aa2b62
Bruners-1308 <43767231+Bruners-1308@users.noreply.github.com>
2019-02-20 22:34:54 -0300

    Add files via upload

--- aula_3/tabela.php
+++ aula_3/tabela.php
@@ -5,21 +5,37 @@
 		<body>
 		
 			<form method="post">		
-				<input placeholder="Insira o número" type="text" name="txNumero1" />
-								
+				<input placeholder="Multiplicador" type="text" name="txNumero1" />
+				<input placeholder="Valor Inicial" type="text" name="txNumero2" />
+				<input placeholder="Valor Final" type="text" name="txNumero3" />
+				
 				<input type="submit" value="Calcular" />
 	
 			</form>
 		
 				<?php
 				
-				$mult= $_POST["txNumero1"];
+				/** $mult= $_POST["txNumero1"];
 							
 				for($i = 0; $i < 11; $i++) {
 				echo "$mult x $i=";
 				echo $mult*$i;
 				echo "<br>";
 				}
+				
+				**/
+			
+				$mult = $_POST["txNumero1"];
+				$i = $_POST["txNumero2"]; 
+				$f= $_POST["txNumero3"]; 
+			
+				
+				for($valor = $i; $valor <= $f; $valor++) {
+				echo "$mult x $valor=";
+				echo $mult*$valor;
+				echo "<br>";
+				}
+				
 				?>
 			
 			
