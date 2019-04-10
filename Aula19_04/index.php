b632558512a5637bcd0314e1bfc685e23a2bbde4
Bruners-1308 <43767231+Bruners-1308@users.noreply.github.com>
2019-04-10 19:58:58 -0300

    Add files via upload

---
+++ Aula19_04/index.php
@@ -0,0 +1,54 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	</head>
+		<body>
+
+
+			<section>			
+				<h1> Exemplo Consulta com banco de dados </h1>			
+			</section>
+			
+			<section>
+			
+				<table class="table table-striped table-dark">
+				  <thead>
+					<tr>
+					  <th scope="col">#</th>
+					  <th scope="col">Data do cadastro</th>
+					  <th scope="col">Usuário</th>
+					  <th scope="col">Senha</th>
+					</tr>
+				  </thead>
+				  <tbody>
+
+				<?php
+					require('conexao.php');
+					
+					$consultaLogin = $pdo->query("SELECT * FROM tbLogin;");
+					
+					while ($resultTbLogin = $consultaLogin->fetch(PDO::FETCH_ASSOC)) {													
+					
+						echo "<tr>";
+						echo "<th scope ='row'> {$resultTbLogin['idLogin']}  </th>";
+						echo "<td> {$resultTbLogin['dtCadastro']} </td>";
+						echo "<td> {$resultTbLogin['Usuario']} </td>";
+						echo "<td> {$resultTbLogin['Senha']} </td>";
+						echo "</tr>";
+					
+					}
+					$pdo = null;	
+				?>
+				
+				
+				  
+	
+				  </tbody>
+				</table>
+					
+			</section>
+			
+		</body>
+</html>
